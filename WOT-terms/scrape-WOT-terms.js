373aba8b3d4d0a2f58b2d623b2dbb666c5d65fa0
kor <kor@dwarshuis.com>
2023-05-16 09:07:00 +0200

    Extra field in the scheme

--- WOT-terms/scrape-WOT-terms.js
+++ WOT-terms/scrape-WOT-terms.js
@@ -3,6 +3,7 @@ import createOutput from '../modules/createOutput.js';
 import createInput from '../modules/createInput.js';
 import writeToFile from '../modules/writeToFile.js';
 
+const siteName = 'This website';
 const url = 'https://weboftrust.github.io/WOT-terms/sitemap.xml';
 const result = '../output/WOT-terms.json';
 
@@ -20,16 +21,16 @@ const result = '../output/WOT-terms.json';
   console.log('Indexing pages...');
 
   // Full index:
-  // for (const url of sitemap.urlset.url) {
-  // Partial index for testing:
-  for (const url of sitemap.urlset.url.slice(100, 103)) {
+  for (const url of sitemap.urlset.url) {
+    // Partial index for testing:
+    // for (const url of sitemap.urlset.url.slice(100, 113)) {
     const pageUrl = url.loc[0];
     console.log(`Indexing ${pageUrl}`);
 
     // Navigate to the page URL and get all paragraph nodes
     await page.goto(pageUrl);
     const elements = await page.$$eval(
-      'article p, article markdown h1, article markdown h2, article markdown h3, article markdown h4, article markdown h5, article markdown a, article markdown li',
+      'article .markdown p, article .markdown h1, article .markdown h2, article .markdown h3, article .markdown h4, article .markdown h5, article .markdown a, article .markdown li',
       (elements) =>
         elements.map((el) => ({
           text: el.textContent.trim(),
@@ -41,11 +42,11 @@ const result = '../output/WOT-terms.json';
     // await page.waitForSelector('ul li.breadcrumbs__item span');
 
     // Find the breadcrumbs element and all its child <li> elements
-    const breadcrumbs = await page.$$eval('.breadcrumbs__link', (nodes) =>
+    const hierarchyLevels = await page.$$eval('.breadcrumbs__link', (nodes) =>
       nodes.map((node) => node.textContent.trim())
     );
 
-    console.log(breadcrumbs);
+    console.log(hierarchyLevels);
 
     const articleExists = await page.$('article');
     let knowledgeLevel;
@@ -59,17 +60,20 @@ const result = '../output/WOT-terms.json';
       console.log('No article element found.');
     }
 
-    entries.push(
-      createOutput({
-        pageUrl,
-        elements: elements,
-        hierarchyLvl0: breadcrumbs[0],
-        hierarchyLvl1: breadcrumbs[1],
-        hierarchyLvl2: breadcrumbs[2],
-        hierarchyLvl3: breadcrumbs[3],
-        knowledgeLevel,
-      })
-    );
+    let output = createOutput({
+      siteName,
+      pageUrl,
+      elements: elements,
+      hierarchyLvl0: hierarchyLevels[0],
+      hierarchyLvl1: hierarchyLevels[1],
+      hierarchyLvl2: hierarchyLevels[2],
+      hierarchyLvl3: hierarchyLevels[3],
+      knowledgeLevel,
+    });
+
+    output.forEach((entry) => {
+      entries.push(entry);
+    });
   }
 
   writeToFile(entries, result);
